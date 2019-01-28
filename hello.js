a04dd296a2c8446aa7e5ddadfe3c65edfcf10e14
Antonio Salgado Lemus <asalgadolemus@una.edu>
2019-01-28 22:45:40 +0000

    daily 1/28/19

---
+++ hello.js
@@ -0,0 +1,15 @@
+const http = require("http");
+
+http.createServer(function(request,response){
+    //console.log(request);
+    response.writeHead(200);
+    response.write("<html><body><p>Hello, dog is running.</p></body></html>");
+    setTimeout(function() {
+        response.write("<html><body><p>dog is really tired</p></body></html>");
+        
+        response.end();
+    },5000);
+    
+}).listen(8080);
+
+console.log('Listening on port 8080...');
